c7526098fb1b49570f5e8feb45d7e1b4f59b0485
Evgenij Semenov <esemenov85@gmail.com>
2025-04-11 16:13:54 +0300

    upd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const updateCursor = ({ x, y }) => {
-  document.documentElement.style.setProperty('--x', x)
-  document.documentElement.style.setProperty('--y', y)
-}
+  document.documentElement.style.setProperty('--x', x);
+  document.documentElement.style.setProperty('--y', y);
+};
 
-document.body.addEventListener('pointermove', updateCursor)
+document.body.addEventListener('pointermove', updateCursor);
